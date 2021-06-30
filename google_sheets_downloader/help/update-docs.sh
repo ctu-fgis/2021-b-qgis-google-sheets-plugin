2f8f44ec4d7958aa63ffbba6e2fff68a9387cabf
Martin Landa <landa.martin@gmail.com>
2021-06-30 18:11:35 +0200

    update docs

--- google_sheets_downloader/help/update-docs.sh
+++ google_sheets_downloader/help/update-docs.sh
@@ -3,13 +3,13 @@
 git checkout gh-pages
 rm -rf *
 touch .nojekyll
-git checkout master help
-cd help
+git checkout master google_sheets_downloader/help
+cd google_sheets_downloader/help
 make clean
 make html
-cd ..
-mv help/build/html/* ./
-rm -rf help
+cd ../..
+mv google_sheets_downloader/help/build/html/* ./
+rm -rf google_sheets_downloader
 git add -A
 git commit -m "publishing updated docs..."
 git push origin gh-pages
